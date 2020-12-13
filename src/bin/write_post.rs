d24c412e76ec0fdd8bffebbb61c82b8878b05f04
mbsd <mbsd@m0x.ru>
2020-12-13 13:03:35 -0300

    write post

---
+++ src/bin/write_post.rs
@@ -0,0 +1,25 @@
+extern crate diesel;
+extern crate rust_diesel;
+
+use self::rust_diesel::*;
+use std::io::{stdin, Read};
+
+const EOF: &str = "CTRL+D";
+
+fn main() {
+    let connection = estabilish_connection();
+
+    println!("What would you like your title to be?");
+    let mut title = String::new();
+    stdin().read_line(&mut title).unwrap();
+    let title = &title[..(title.len() - 1)]; // drop new line
+    println!(
+        "\nOk! Let's write {} (Press {} when finished)\n",
+        title, EOF
+    );
+    let mut body = String::new();
+    stdin().read_to_string(&mut body).unwrap();
+
+    let post = create_post(&connection, title, &body);
+    println!("\nSaved draft {} with id {}", title, post.id);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,25 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
+use self::models::{NewPost, Post};
+
 pub fn estabilish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connection to {}", database_url))
+}
+
+pub fn create_post(conn: &PgConnection, title: &str, body: &str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
 }
 
 #[cfg(test)]
