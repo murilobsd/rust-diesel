c44fbcdceebeb111d3794362de1406a3cd88c0a8
mbsd <mbsd@m0x.ru>
2020-12-13 13:12:50 -0300

    binary to publish post (update from published false to true)

---
+++ src/bin/publish_post.rs
@@ -0,0 +1,25 @@
+extern crate diesel;
+extern crate rust_diesel;
+
+use self::diesel::prelude::*;
+use self::models::Post;
+use self::rust_diesel::*;
+use std::env::args;
+
+fn main() {
+    use rust_diesel::schema::posts::dsl::{posts, published};
+
+    let id = args()
+        .nth(1)
+        .expect("publish_post requires a post id")
+        .parse::<i32>()
+        .expect("invalid id");
+    let connection = estabilish_connection();
+
+    let post = diesel::update(posts.find(id))
+        .set(published.eq(true))
+        .get_result::<Post>(&connection)
+        .expect(&format!("unable to find post {}", id));
+
+    println!("Published post {}", post.title);
+}
